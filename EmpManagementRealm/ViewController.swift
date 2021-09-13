4601c8af37b752078588a34d8714deaa92f9bc1f
Ritika Verma <ritika.v2000@gmail.com>
2021-09-13 10:30:32 +0530

    Initial Commit

---
+++ EmpManagementRealm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmpManagementRealm
+//
+//  Created by RITIKA VERMA on 13/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
